5d0eddc2125102e4eb045167a1500933d77a224e
781enes <63649659+781enes@users.noreply.github.com>
2022-06-11 21:42:43 +0300

    Add files via upload

---
+++ XOX new/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace XOX_new
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 70);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 70);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(158, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 70);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(158, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 70);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(82, 146);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 70);
+            this.button5.TabIndex = 3;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 146);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 70);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(158, 222);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 70);
+            this.button7.TabIndex = 5;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(82, 222);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 70);
+            this.button8.TabIndex = 5;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(6, 222);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 70);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(90, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "O";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 297);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "X - O - X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ XOX new/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOX_new
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (label1.Text == "X")
+            {
+
+                button.Text = "X";
+                button.Enabled = false;
+                label1.Text = "O";
+            }
+
+            else
+            {
+                button.Text = "O";
+                button.Enabled = false;
+                label1.Text = "X";
+            }
+            // OX 1
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+            {
+                MessageBox.Show("Oyunu : X  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+
+            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+            // OX 2
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                MessageBox.Show("Oyunu : X  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+            if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+            // OX 3
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+            if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+            //OX4
+            if (button1.Text == "X" && button6.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Oyunu : X  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+            if (button1.Text == "O" && button6.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+            //OX5
+            if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+            {
+                MessageBox.Show("Oyunu : X  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+            if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+            //OX6
+            if (button3.Text == "X" && button4.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("Oyunu : X  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+
+            if (button3.Text == "O" && button4.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+            // OX 7
+
+            if (button3.Text == "X" && button5.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Oyunu : X  Kazandı", "XOX System");
+                OyunSonu();
+            }
+
+            if (button3.Text == "O" && button5.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Oyunu : O  Kazandı", "XOX System");
+                OyunSonu();
+            }
+            //OX8
+
+            if (button1.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("Oyunu kazanan : X", "XOX System");
+                OyunSonu();
+            }
+
+            if (button1.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("Oyunu kazanan : O", "XOX System");
+                OyunSonu();
+            }
+            if (button1.Text != "" && button2.Text != ""&& button3.Text != "" && button4.Text !="" && button5.Text != "" &&
+                button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "")
+            {
+                DialogResult dialog = new DialogResult();
+
+                dialog = MessageBox.Show("Oyunu Kimse kazanamadı. Tekrar denemek ister misiniz?", "XOX System", MessageBoxButtons.RetryCancel, MessageBoxIcon.Question);
+                if (dialog == DialogResult.Retry)
+                {
+                    MessageBox.Show("Oyun yeniden başlatılıyor, Bekleyiniz...", "XOX System");
+                    OyunSonu();
+                }
+
+                else
+                {
+                    MessageBox.Show("Oyun kapatılıyor...", "XOX System");
+                    this.Close();
+                }
+            }
+        }
+        private void OyunSonu()
+        {
+            label1.Text = "X";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+    }
+}
